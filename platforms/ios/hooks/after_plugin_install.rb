358d4abdc90ed4293c120a87e385c68ba982a6fb
Kunio Sawatani <devel@fathens.org>
2016-12-26 22:41:28 +0900

    hook は不要

--- platforms/ios/hooks/after_plugin_install.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'pathname'
-require 'fetch_local_lib'
-
-puts "Working with #{$0}"
-
-$PLUGIN_DIR = Pathname(ENV['CORDOVA_HOOK'] || $0).realpath.dirname.dirname.dirname.dirname
